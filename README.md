27863d540dc93a4879397fe955726f44904041ce
Andy Motta <andylmotta@gmail.com>
2017-06-13 18:03:17 -0700

    Initial Source Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ansible-iam-key-rotation
-Ansible/Boto3 Role to do IAM Key Rotation on target
+
+This is a Boto3 Script to rotate AWS IAM keys on EC2 targets with an Ansible wrapper for use with dynamic inventory.
+
+The scripts assumes the Ansible control node has admin keys with privileges to rotate remote/target IAM keys.  It will loop through profiles on the control node for access, and look for matching profiles on the target node for rotation.
+
+For example,  if the control node has the profile prod and the target node also has a profile named prod, those keys will be rotated as long as the control node has an admin policy for  IAM control.  Access is not required on the target.
+
+Because of this behaviour, we can change *hosts* to **localhost** to target the local `~/.aws/credentials` for both rotation and access.
+
+Usage
+----------------
+(Dynamic Inventory)
+```yml
+- name: Rotate keys on target hosts
+  hosts: "tag_Name_{{ resource_name }}"
+  gather_facts: true
+  become: true
+  become_user: root
+  roles:
+    # IAM Key Rotation
+    - { role: rotation, tags: rotation }
+```
---
+++ rotation/files/rotate.py
@@ -0,0 +1,127 @@
+import boto3
+from botocore.exceptions import ClientError
+import logging
+import os
+from datetime import datetime
+import shutil
+from ConfigParser import SafeConfigParser
+
+logging.basicConfig()
+logger = logging.getLogger()
+
+key_file = os.path.join(os.environ['HOME'], 'creds')
+access_file = os.path.join(os.environ['HOME'], '.aws', 'credentials')
+
+parser = SafeConfigParser()
+parser.read(key_file)
+access_list = SafeConfigParser()
+access_list.read(access_file)
+
+timeStamp =  datetime.fromtimestamp(os.path.getmtime(key_file)).strftime("%b-%d-%y-%H:%M:%S")
+key_bak = "%s_%s.bak" % (key_file, timeStamp)
+
+def generate_profile_list():
+    lst = []
+    for profile in parser.sections():
+        lst.append(profile)
+    return lst
+
+def generate_access_list():
+    lst = []
+    for profile in access_list.sections():
+        lst.append(profile)
+    return lst
+
+def get_aws_access_key_id(profile):
+    return parser.get(profile, 'aws_access_key_id')
+
+def find_user(key):
+    users = iam.list_users()["Users"]
+    for user in users:
+        username = user['UserName']
+        for key_result in iam.list_access_keys(UserName=username)['AccessKeyMetadata']:
+            aws_access_key = key_result['AccessKeyId']
+            if aws_access_key == key:
+                return username
+    return "No match found"
+
+def num_keys():
+    # See if IAM user already has more than one key
+    paginator = iam.get_paginator('list_access_keys')
+    try:
+        for response in paginator.paginate(UserName=user):
+            return len(response['AccessKeyMetadata'])
+    except ClientError as e:
+        logger.error("Received error: %s", e, exc_info=True)
+        if e.response['Error']['Code'] == 'ParamValidationError':
+            raise
+
+# Create an access key
+def create_access_key(user):
+    try:
+        response = iam.create_access_key(
+            UserName=user
+        )
+        AccessKey = response['AccessKey']['AccessKeyId']
+        SecretAccessKey = response['AccessKey']['SecretAccessKey']
+        return AccessKey, SecretAccessKey
+    except ClientError as e:
+        logger.error("Received error: %s", e, exc_info=True)
+        if e.response['Error']['Code'] == 'LimitExceededException':
+            print "User already has two keys, cannot add more"
+
+# Change state of first access key to inactive before deleting
+# this should only tru to update users with one key
+def update_access_key(key, user):
+    iam.update_access_key(
+            AccessKeyId=key,
+            Status='Inactive',
+            UserName=user
+        )
+
+def write_creds(profile, keyid, secret):
+    parser.set(profile, 'aws_access_key_id', keyid)
+    parser.set(profile, 'aws_secret_access_key', secret)
+    # Writing our configuration file to 'example.ini'
+    with open(key_file, 'wb') as configfile:
+        parser.write(configfile)
+
+#### This should be the start of the main function ####
+# first create backup
+shutil.copy(key_file, key_bak)
+
+# 1. For each profile in AWS credentials, get user.
+profiles = generate_profile_list()
+access = generate_access_list()
+keys = []
+for p in profiles:
+    if p == 'default':
+        continue
+    if p not in access:
+        print "Not rotating %s profile, no access." % p
+        continue
+    key = get_aws_access_key_id(p)
+    if key in keys: # Don't do this twice if default is the same key as some other profile
+        print "Will not rotate %s, list duplicate." % p
+        continue
+    keys.append(key)
+    # create a custom session to taget account based on profile
+    os.environ["AWS_PROFILE"] = p
+    session = boto3.session.Session()
+    iam = session.client('iam')
+    # Get the user from the key on the host
+    user = find_user(key)
+    # 2. Can we add a key for this user?  If yes, create key.  If not, log.
+    if num_keys() == 2:
+        print "Cannot add more keys for " + user + " " + key
+        continue
+    print p +": " + key + " " + user, num_keys()
+    # 3. Add a secondary key for the users we can add keys to
+    creds = create_access_key(user)
+    print "Created: " + creds[0] + "in: " + p
+    # 5. deactivate the original keys for each user
+    update_access_key(key, user)
+    print "Successfully deactivated " + key + " in " + p
+    # 6. rotate user and secret of each profile
+    print "Writing creds to " + key_file + "..."
+    write_creds(p, creds[0], creds[1])
